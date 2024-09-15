585409de5466ec8010a5e397cc43e060ed6fe5d7
yatharth srivastava <yatharthsriv18@gmail.com>
2024-09-15 06:53:46 +0530

    dep

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,8 +2,6 @@
 import Footer from "@/components/Footer/page";
 import Hero from "@/components/Hero/page";
 import Navbar from "@/components/Navbar/page";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
